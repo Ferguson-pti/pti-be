506261007b2d2f2266857f19015804704ccc6025
Chukwu Rophi <chukwurophi@gmail.com>
2025-05-15 17:34:30 +0100

    feat: intialized project and setup the abstracts and messages components

---
+++ src/api/abstract/controllers/abstract.ts
@@ -0,0 +1,7 @@
+/**
+ * abstract controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::abstract.abstract');
---
+++ src/api/abstract/services/abstract.ts
@@ -0,0 +1,7 @@
+/**
+ * abstract service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::abstract.abstract');
